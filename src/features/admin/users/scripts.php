8e73ba5894e48a51b3504262f2cb2a85ab5f00bd
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-04-18 01:22:33 +0800

    fix: users page on admin

---
+++ src/features/admin/users/scripts.php
@@ -0,0 +1,11 @@
+<!-- Required for DataTables -->
+<script src="<?= statf('lib/DataTables/datatables.min.js') ?>"></script>
+<script src="<?= statf('lib/DataTables/dataTables.semanticui.min.js') ?>"></script>
+<script src="<?= statf('lib/DataTables/responsive.semanticui.min.js') ?>"></script>
+
+<script src="<?= utils('js/formatters.js', true) ?>"></script>
+<script src="<?= shared('assets/js/tableListFilters.js', true) ?>"></script>
+
+<!-- Custom Scripts -->
+<script src="<?= featured('admin/users/assets/js/usersDataTable.js', true) ?>"></script>
+<script src="<?= featured('admin/users/assets/js/validateUserForm.js', true) ?>"></script>
