289a5a234342bf4eef586d5ea299902ef37f3ca1
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-04-18 16:48:40 +0800

    changes: make the /user front end ready

--- src/shared/elements/styles.php
+++ src/shared/elements/styles.php
@@ -7,6 +7,7 @@ $styles = [
     'vendor/bootstrap/dist/css/bootstrap-grid.min.css',
     'vendor/bootstrap/dist/css/bootstrap-utilities.min.css',
     'vendor/bootstrap/dist/css/bootstrap-reboot.min.css',
+    // 'vendor/bootstrap/dist/css/bootstrap.min.css',
     'vendor/fomantic-ui/dist/semantic.min.css',
     'assets/css/global.css',
     'assets/css/loader/window.css',
