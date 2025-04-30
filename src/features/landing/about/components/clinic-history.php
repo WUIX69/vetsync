4890b305879db98c0532492ea5c66f60e65b6b9c
Code-JM <tubanajordanmiguel@gmail.com>
2025-04-30 12:14:09 +0800

    Update: Moved <Styles> inside components of about.php landing

--- src/features/landing/about/components/clinic-history.php
+++ src/features/landing/about/components/clinic-history.php
@@ -1,3 +1,36 @@
+<style>
+    /*----------- MAIN (Clinic History) -----------*/
+    main .section-container:has(section.clinic-history) {
+        background: var(--color-white);
+    }
+
+    main section.clinic-history .history-image {
+        width: 100%;
+        border-radius: 10px;
+        box-shadow: var(--box-shadow);
+    }
+
+    main section.clinic-history .history-image img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+    }
+
+    main section.clinic-history h2 {
+        color: var(--color-dark);
+        font-size: 2rem;
+        margin-bottom: 25px;
+        font-weight: 600;
+    }
+
+    main section.clinic-history p {
+        color: var(--color-dark-variant);
+        line-height: 1.8;
+        font-size: 1.1rem;
+        margin-bottom: 15px;
+    }
+</style>
 <!-- Clinic History -->
 <div class="section-container">
     <section class="section-wrapper clinic-history">
