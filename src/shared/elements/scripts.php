4c52fc638d37047d5f7044cba5bf18679b3d2a11
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-05-09 16:08:02 +0800

    fix: redundant scripts errors

--- src/shared/elements/scripts.php
+++ src/shared/elements/scripts.php
@@ -3,35 +3,49 @@
 $appUrlPath = $_SERVER['REQUEST_URI'] ?? null;
 $appDirName = explode('/', trim($appUrlPath, '/'))[2] ?? '';
 
-// All scripts needed by the application
-$scripts = [
-    // Regular scripts
-    ['src' => 'lib/jquery/jquery.min.js', 'module' => false, 'utility' => false],
-    ['src' => 'vendor/fomantic-ui/dist/semantic.min.js', 'module' => false, 'utility' => false],
+rcsScripts();
+function rcsScripts()
+{
+    // All scripts needed by the application
+    $scripts = [
+        // Regular scripts
+        ['src' => 'lib/jquery/jquery.min.js', 'module' => false],
+        ['src' => 'vendor/fomantic-ui/dist/semantic.min.js', 'module' => false],
 
-    // Bootstrap modules (loaded as ES modules)
-    ['src' => 'vendor/bootstrap/js/src/base-component.js', 'module' => true, 'utility' => false],
-    ['src' => 'vendor/bootstrap/js/src/button.js', 'module' => true, 'utility' => false],
-    ['src' => 'vendor/bootstrap/js/src/collapse.js', 'module' => true, 'utility' => false],
-    ['src' => 'vendor/bootstrap/js/src/tab.js', 'module' => true, 'utility' => false],
+        // Bootstrap modules (loaded as ES modules)
+        ['src' => 'vendor/bootstrap/js/src/base-component.js', 'module' => true],
+        ['src' => 'vendor/bootstrap/js/src/button.js', 'module' => true],
+        ['src' => 'vendor/bootstrap/js/src/collapse.js', 'module' => true],
+        ['src' => 'vendor/bootstrap/js/src/tab.js', 'module' => true],
 
-    // Regular scripts
-    ['src' => 'lib/lodash/lodash.min.js', 'module' => false, 'utility' => false],
-    ['src' => 'js/loader/window.js', 'module' => false, 'utility' => false],
-    ['src' => 'js/darkmode.js', 'module' => false, 'utility' => false],
-    ['src' => 'js/' . $appDirName . '/script.js', 'module' => false, 'utility' => false],
-    ['src' => 'js/' . $appDirName . '/prefetch.js', 'module' => false, 'utility' => false],
-    ['src' => 'js/scripts.js', 'module' => false, 'utility' => false],
+        // Regular scripts
+        ['src' => 'lib/lodash/lodash.min.js', 'module' => false],
+        ['src' => 'js/loader/window.js', 'module' => false],
+        ['src' => 'js/darkmode.js', 'module' => false],
+        // ['src' => 'js/' . $appDirName . '/script.js', 'module' => false],
+        // ['src' => 'js/' . $appDirName . '/prefetch.js', 'module' => false],
+        ['src' => 'js/scripts.js', 'module' => false],
+    ];
 
-    // Utils
-    ['src' => 'js/validateHandler.js', 'module' => false, 'utility' => true],
-    ['src' => 'js/middleware.js', 'module' => false, 'utility' => true],
-];
+    foreach ($scripts as $script) {
+        $moduleAttr = $script['module'] ? ' type="module"' : '';
+        echo '<script' . $moduleAttr . ' src="' . asset($script['src']) . '"></script>';
+    }
+}
 
-foreach ($scripts as $script) {
-    $moduleAttr = $script['module'] ? ' type="module"' : '';
-    $srcRef = $script['utility'] ? utils($script['src'], true) : asset($script['src']);
-    echo '<script' . $moduleAttr . ' src="' . $srcRef . '"></script>';
+
+utilScripts();
+function utilScripts()
+{
+    // Utility scripts
+    $utilityScripts = [
+        'js/validateHandler.js',
+        'js/middleware.js',
+    ];
+
+    foreach ($utilityScripts as $script) {
+        echo '<script src="' . utils($script, true) . '"></script>';
+    }
 }
 
 ?>
