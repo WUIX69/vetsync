9ded73ac5d3464f47254a4d1cb42facff3566752
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-05-11 11:43:22 +0800

    refactor: re-usable google map for /landing

--- src/features/landing/home/components/location.php
+++ src/features/landing/home/components/location.php
@@ -234,12 +234,8 @@
                                 Monte Bulacan
                             </div>
                         </div>
-                        <div class="google-map">
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d246885.5345714357!2d121.0624292930067!3d14.79230698166309!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397af80f101e06b%3A0x63c98de46231d439!2sJoseph%20Z.%20Angeles%20Veterinary%20Clinic!5e0!3m2!1sen!2sph!4v1743320746862!5m2!1sen!2sph"
-                                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
-                                referrerpolicy="no-referrer-when-downgrade"></iframe>
-                        </div>
+                        <!-- Google Map Iframe -->
+                        <?= featured('landing/shared/components/ui/google-map'); ?>
                     </div>
                 </div>
             </div>
