bad4b2621d4c853467595e8521b5578f1ac8e73d
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-05-17 14:53:05 +0800

    changes: improved php-set

--- src/core/php-set.php
+++ src/core/php-set.php
@@ -7,17 +7,15 @@ ini_set('upload_max_filesize', '10M');
 ini_set('post_max_size', '10M');
 
 // PHP Error Reporting
+ini_set('log_errors', 1);
+ini_set('error_log', $config['root_path'] . '/logs/error.log');
 if ($_ENV['APP_DEBUG']) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-    ini_set('log_errors', 1);
-    ini_set('error_log', $config['root_path'] . '/logs/error.log');
 } else {
     ini_set('display_errors', 0);
     ini_set('display_startup_errors', 0);
-    ini_set('log_errors', 1);
-    ini_set('error_log', $config['root_path'] . '/logs/error.log');
 }
 
 // Set timezone
