59f4d35135edf89c11fbae0329ec6f7bbc6f362c
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-05-17 16:55:32 +0800

    changes: updt /auth continue-btn component

--- src/features/auth/shared/components/ui/continue-btn.php
+++ src/features/auth/shared/components/ui/continue-btn.php
@@ -1,10 +1,4 @@
-<?php
-
-$urlPath = $_SERVER['REQUEST_URI'] ?? null;
-$dirName = explode('/', trim($urlPath, '/'))[3] ?? '';
-$pageName = str_replace('.php', '', $dirName);
-
-?>
+<?php $pageName = uriPagePath(); ?>
 <button class="ui fluid large teal submit button" type="submit">
     <?= $pageName === 'register' ? 'Register' : 'Continue' ?>
 </button>
