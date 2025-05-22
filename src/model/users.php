74dc639b8a4a50a2e5e0ffc9e62ee035d9f616e1
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-05-22 12:08:51 +0800

    changes: updt users model to add try catch block

--- src/model/users.php
+++ src/model/users.php
@@ -12,16 +12,26 @@ class Users
 
     public function single($uuid = null)
     {
-        $stmt = $this->conn->prepare('SELECT * FROM users WHERE uuid=? LIMIT 1');
-        $stmt->execute([$uuid]);
-        return $stmt->fetch(PDO::FETCH_ASSOC) ?? [];
+        try {
+            $stmt = $this->conn->prepare('SELECT * FROM users WHERE uuid=? LIMIT 1');
+            $stmt->execute([$uuid]);
+            return $stmt->fetch(PDO::FETCH_ASSOC) ?? [];
+        } catch (PDOException $e) {
+            error_log("SQL Error: " . $e->getMessage());
+            return [];
+        }
     }
 
     public function singleWhereUserEmail($email = null)
     {
-        $stmt = $this->conn->prepare("SELECT * FROM users WHERE email=? LIMIT 1");
-        $stmt->execute([$email]);
-        return $stmt->fetch(PDO::FETCH_ASSOC) ?? [];
+        try {
+            $stmt = $this->conn->prepare("SELECT * FROM users WHERE email=? LIMIT 1");
+            $stmt->execute([$email]);
+            return $stmt->fetch(PDO::FETCH_ASSOC) ?? [];
+        } catch (PDOException $e) {
+            error_log("SQL Error: " . $e->getMessage());
+            return [];
+        }
     }
 
     public function singleWhereAdminEmail($email = null)
