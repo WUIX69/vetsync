67ca2c41307f65e993c0adfad197d8387d5db884
WUIX69 <81736347+WUIX69@users.noreply.github.com>
2025-06-06 22:22:35 +0800

    build: dataTables shared elements

---
+++ src/shared/elements/dataTables/scripts.php
@@ -0,0 +1,4 @@
+<script src="<?= asset('lib/DataTables/datatables.min.js') ?>"></script>
+<script src="<?= asset('lib/DataTables/dataTables.semanticui.min.js') ?>"></script>
+<script src="<?= asset('lib/DataTables/dataTables.responsive.min.js') ?>"></script>
+<script src="<?= asset('lib/DataTables/responsive.semanticui.min.js') ?>"></script>
---
+++ src/shared/elements/dataTables/styles.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="<?= asset('lib/DataTables/datatables.min.css') ?>">
+<link rel="stylesheet" href="<?= asset('lib/DataTables/dataTables.semanticui.min.css') ?>">
+<link rel="stylesheet" href="<?= asset('lib/DataTables/responsive.semanticui.min.css') ?>">
