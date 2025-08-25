bde4a50c3163e5a6ed721f6455bfda8a654e2976
Code-JM <tubanajordanmiguel@gmail.com>
2025-08-25 22:37:19 +0800

    UPDATE: Real time update for products user page

--- src/app/user/products.php
+++ src/app/user/products.php
@@ -7,16 +7,17 @@
     <?= shared('elements/meta'); ?> <!-- rcs Meta -->
     <title>Products - VetSync</title>
     <?= shared('elements/styles'); ?> <!-- rcs Styles -->
-    <style></style>
+    <!-- Add Boxicons CSS -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
 
 <body>
     <div class="shared-standalone-content">
-        <?= shared('layouts/loader/window'); ?>
-        <?= shared('layouts/top-redirect-btn'); ?>
+        <?= shared('layouts/loader/window'); ?> <!-- Window Spinner -->
+        <?= shared('layouts/top-redirect-btn'); ?> <!-- Top Redirect Button -->
     </div>
 
-    <div class="container-body">
+    <div class="site-cont">
         <!-- Site Header -->
         <?= partial('layouts/header'); ?>
 
@@ -29,12 +30,8 @@
         </main>
     </div>
 
-    <?= shared('elements/scripts'); ?>
-    <script type="text/javascript">
-        $(function () {
-            $('.ui.rating').rating();
-        });
-    </script>
+    <?= shared('elements/scripts'); ?> <!-- rcs Scripts -->
+    <script src="/src/features/products/js/user-products-list.js"></script>
 </body>
 
 </html>
